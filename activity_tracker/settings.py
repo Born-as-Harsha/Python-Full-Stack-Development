adf0a928deb83d413221951fa1a09889dd29d56a
Born-as-Harsha <harshamass2007@gmail.com>
2026-04-27 21:35:42 +0530

    Added Procfile correctly

--- activity_tracker/settings.py
+++ activity_tracker/settings.py
@@ -1,12 +1,13 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # 🔐 SECURITY
 SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxxxxxxxxxx'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 # 🔹 APPLICATIONS
 INSTALLED_APPS = [
@@ -24,6 +25,7 @@ INSTALLED_APPS = [
 # 🔹 MIDDLEWARE
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,10 +58,7 @@ WSGI_APPLICATION = 'activity_tracker.wsgi.application'
 
 # 🔹 DATABASE
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # 🔹 PASSWORD VALIDATION
@@ -78,9 +77,7 @@ USE_TZ = True
 
 # 🔹 STATIC FILES
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "tracker" / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # 🔹 MEDIA FILES
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
